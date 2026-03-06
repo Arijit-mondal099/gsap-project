7f0dc954b31c0bee02b7c9592414b9f246b38133
Arijit Mondal <arijitm717@gmail.com>
2026-03-06 23:50:49 +0530

    gsap setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+// register gsap plugins to access those globaly
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
