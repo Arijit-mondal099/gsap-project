4127c388c76580ced1367ba594b606aa0dc733b6
Arijit Mondal <arijitm717@gmail.com>
2026-03-07 10:45:00 +0530

    added first part for hero section

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/common/Navbar";
+import Hero from "./components/sections/Hero";
 
 // register gsap plugins to access those globaly
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <main className="h-screen">
+    <main>
       <Navbar />
+      <Hero />
+      <div className="min-h-screen"></div>
     </main>
   );
 };
